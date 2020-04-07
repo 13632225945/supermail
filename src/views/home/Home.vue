d49e5b931504aca52029699ca51213a5a5fcb74b
mho <969407578@qq.com>
2020-04-07 16:33:24 +0800

    '首页头部搜索框'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,15 @@
 <template>
-  <div>首页</div>
+  <div>
+    <search-bar></search-bar>
+  </div>
 </template>
 
 <script>
+import SearchBar from './searchbar/SearchBar'
 export default {
-
+  components: {
+    SearchBar
+  }
 }
 </script>
 
