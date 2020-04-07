db4273d3833738bf7895e22eac4a6b8d5ee896cf
mho <969407578@qq.com>
2020-04-07 10:37:34 +0800

    '菜单栏'

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
-
+<script>
+import MainTabBar from 'components/content/maintabbar/MainTabBar'
+export default {
+  components: {
+    MainTabBar
+  }
+}
+</script>
 <style>
 @import "assets/css/base.css";
 </style>
